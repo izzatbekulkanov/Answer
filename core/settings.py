5e364cef20dfd59d290c348dc42eac13b32796ca
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2025-05-31 22:42:37 +0500

    s

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://test.ulkanov.uz",
+]
+
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",
