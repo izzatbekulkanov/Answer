2349b0dd71fa18ad6d466c9ff9ecb7017381fe5f
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2025-06-01 16:19:58 +0500

    ss

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('api/search', views.search_question, name='search_question'),
     path('api/questions/', QuestionListView.as_view(), name='question_list'),
     path('users/', views.users_list, name='users'),
+    path('users/detail/<int:user_id>/', views.user_detail, name='user_detail'),
     path('add-user/', views.add_user, name='add_user'),
     path('check-username/', views.check_username, name='check_username'),
     path('check-personal-code/', views.check_personal_code, name='check_personal_code'),
     path('delete-user/<int:user_id>/', views.delete_user, name='delete_user'),
     path('reset-password/<int:user_id>/', views.reset_password, name='reset_password'),
-    path('user/<int:user_id>/', views.user_detail, name='user_detail'),
     path('clean-duplicates/', views.clean_duplicates_view, name='clean_duplicates'),
     path('accounts/', include('django.contrib.auth.urls')),  # Django autentifikatsiyasi va logout
 ]
